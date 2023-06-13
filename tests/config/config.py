2a319100ff720db713bf06a1e9f9c8d98ce4d540
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2023-06-13 22:48:47 +0200

    minor changes

--- tests/config/config.py
+++ tests/config/config.py
@@ -10,25 +10,22 @@ parser.add_argument("--interactive", action="store_true")
 parser.add_argument("--no-interactive", dest="interactive", action="store_false")
 parser.set_defaults(interactive=True)
 
-parser.add_argument("-a", "--address", default="http://hopaas.cloud.infn.it")
-parser.add_argument("-p", "--port", default=80)
+parser.add_argument(
+    "-s", "--server", default="http://hopaas.cloud.infn.it:11756"
+)  # https://hopaas.cloud.infn.it
 parser.add_argument("-t", "--token", default="user-api-token")
 config_hopaas = dict()
 
 args = parser.parse_args()
 
 if args.interactive:
-    address = input(f"Address for the Hopaas service (default: '{args.address}'): ")
-    config_hopaas["address"] = address if not (address == "") else args.address
+    server = input(f"Address of the Hopaas service (default: '{args.server}'): ")
+    config_hopaas["server"] = server if not (server == "") else args.server
 
-    port = input(f"Port for the Hopaas service (default: '{args.port}'): ")
-    config_hopaas["port"] = port if not (port == "") else args.port
-
-    token = input(f"API token for the Hopaas service (default: '{args.token}'): ")
+    token = input(f"API token to access the Hopaas service (default: '{args.token}'): ")
     config_hopaas["token"] = token if not (token == "") else args.token
 else:
-    config_hopaas["address"] = args.address
-    config_hopaas["port"] = args.port
+    config_hopaas["server"] = args.server
     config_hopaas["token"] = args.token
 
 with open(f"{here}/hopaas.yml", "w") as file:
