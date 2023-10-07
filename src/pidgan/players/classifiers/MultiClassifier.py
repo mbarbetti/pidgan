35d8608f0e5e9f5bd3a8153bf59be5cabfd23ed6
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2023-10-08 00:57:56 +0200

    added multi-class classifier

--- src/pidgan/players/classifiers/MultiClassifier.py
+++ src/pidgan/players/classifiers/MultiClassifier.py
@@ -3,9 +3,10 @@ import tensorflow as tf
 from pidgan.players.discriminators import Discriminator
 
 
-class Classifier(Discriminator):
+class MultiClassifier(Discriminator):
     def __init__(
         self,
+        num_multiclasses,
         num_hidden_layers=5,
         mlp_hidden_units=128,
         dropout_rate=0.0,
@@ -13,11 +14,11 @@ class Classifier(Discriminator):
         dtype=None,
     ) -> None:
         super().__init__(
-            output_dim=1,
+            output_dim=num_multiclasses,
             num_hidden_layers=num_hidden_layers,
             mlp_hidden_units=mlp_hidden_units,
             dropout_rate=dropout_rate,
-            output_activation="sigmoid",
+            output_activation="softmax",
             name=name,
             dtype=dtype,
         )
@@ -27,3 +28,7 @@ class Classifier(Discriminator):
             "Only the `discriminators` family has the "
             "`hidden_feature()` method implemented."
         )
+
+    @property
+    def num_multiclasses(self) -> int:
+        return self._output_dim
--- src/pidgan/players/classifiers/__init__.py
+++ src/pidgan/players/classifiers/__init__.py
@@ -1,1 +1,2 @@
 from .Classifier import Classifier
+from .MultiClassifier import MultiClassifier
