48bfc2047ef052251e5593a843d2b5f899449c0e
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2023-12-28 13:44:55 +0100

    fixed bug with CT inversion

--- src/pidgan/utils/preprocessing/invertColumnTransformer.py
+++ src/pidgan/utils/preprocessing/invertColumnTransformer.py
@@ -5,19 +5,22 @@ from sklearn.compose import ColumnTransformer
 def invertColumnTransformer(column_transformer, preprocessed_X):
     assert isinstance(column_transformer, ColumnTransformer)
 
-    preprocessed_split = {
-        name: [None] * len(cols) for name, _, cols in column_transformer.transformers_
-    }
+    iCol = 0
+    postprocessed_split = dict()
+    for name, algo, cols in column_transformer.transformers_:
+        preprocessed_cols = list()
+        for _ in range(len(cols)):
+            preprocessed_cols.append(preprocessed_X[:, iCol][:, None])
+            iCol += 1
+        preprocessed_block = np.concatenate(preprocessed_cols, axis=1)
+        if algo == "passthrough":
+            postprocessed_split[name] = preprocessed_block
+        else:
+            postprocessed_split[name] = algo.inverse_transform(preprocessed_block)
 
-    for iCol in range(preprocessed_X.shape[1]):
-        name, _, cols = [
-            (n, t, list(c)) for n, t, c in column_transformer.transformers_ if iCol in c
-        ].pop()
-        preprocessed_split[name][cols.index(iCol)] = preprocessed_X[:, iCol]
-
-    X = list()
-    for name, algo, _ in column_transformer.transformers_:
-        split = np.stack(preprocessed_split[name], axis=1)
-        X.append(split if algo == "passthrough" else algo.inverse_transform(split))
+    X = [None] * preprocessed_X.shape[1]
+    for name, _, cols in column_transformer.transformers_:
+        for i, iCol in enumerate(cols):
+            X[iCol] = postprocessed_split[name][:, i][:, None]
 
     return np.concatenate(X, axis=1)
